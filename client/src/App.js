a8819ac10a24334b93308ad1232dd0edc849c0a8
Leon Djusberg <leondjust@me.com>
2021-07-21 01:27:27 +0200

    tired and drunk

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,14 @@ function App() {
         <div className="App">
             <Layout style={{ minHeight: '100vh' }}>
                 <Nav />
-                <Content style={{ margin: '0 16px' }}>
-                    <Main />
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    created by Jared Dye, Leon Djusberg, and Adam Gilbert
-                </Footer>
+                <Layout>
+                    <Content style={{ padding: '16px 16px', margin: '16px 16px 0 16px', backgroundColor: '#454545' }}>
+                        <Main />
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        created by <a href='https://github.com/LeonDJ7'>Jared Dye</a>, <a href='https://github.com/LeonDJ7'>Leon Djusberg</a>, and <a href='https://github.com/adamgilbert912'>Adam Gilbert</a>
+                    </Footer>
+                </Layout>
             </Layout>
         </div>
     )
--- client/src/components/nav/Nav.tsx
+++ client/src/components/nav/Nav.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import 'antd/dist/antd.css'
-import { Layout, Menu, Space } from 'antd'
-import { PieChartOutlined, DesktopOutlined } from '@ant-design/icons'
+import { Layout, Menu } from 'antd'
+import { BarChartOutlined, SlidersOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
 import './Nav.css'
 
 const { Sider } = Layout;
@@ -20,32 +20,40 @@ const Nav: React.FC<Props> = (props) => {
 
     return (
 
-        <React.Fragment>
-
-            <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            <Sider id='nav-root' collapsible collapsed={collapsed} onCollapse={onCollapse}>
                 <img className="logo" src='' alt='' />
-                <Menu theme="dark" defaultSelectedKeys={['3']} mode="inline">
-                    <Link to='/statistics'>
-                        <Menu.Item key="1" icon={<PieChartOutlined />}> Statistics </Menu.Item>
-                    </Link>
-                    <Link to='/recommended'>
-                        <Menu.Item key="2" icon={<DesktopOutlined />}> Recommended </Menu.Item>
-                    </Link>
-                    <Space id='space' />
-                    { collapsed &&
-                        <Link to='/' id='nav-options-container' style={{flexDirection: 'column'}}>
-                            <Menu.Item key="3" icon={<PieChartOutlined />}></Menu.Item>
-                        </Link>
-                    }
-                    { !collapsed &&
-                        <Link to='/' id='nav-options-container' style={{flexDirection: 'row'}}>
-                            <Menu.Item key="4" icon={<DesktopOutlined />}></Menu.Item>
-                        </Link>
-                    }
+                <Menu id='nav-menu' theme='light' defaultSelectedKeys={['3']} mode="inline">
+
+                    <Menu.Item key="1" >
+                        <NavLink className='nav-option-title' to='/statistics'>
+                            <BarChartOutlined />
+                            <span> Statistics </span>
+                        </NavLink>
+                    </Menu.Item>
+
+                    <Menu.Item key="2" >
+                        <NavLink className='nav-option-title' to='/recommended'>
+                            <SlidersOutlined />
+                            <span> Recommended </span>
+                        </NavLink>
+                    </Menu.Item>
+
+                    <Menu.Item key="3" >
+                        <NavLink className='nav-option-title' to='/'>
+                            <UserOutlined />
+                            <span> Profile </span>
+                        </NavLink>
+                    </Menu.Item>
+
+                    <Menu.Item key="4" >
+                        <NavLink className='nav-option-title' to='/contact'>
+                            <MailOutlined />
+                            <span> Contact </span>
+                        </NavLink>
+                    </Menu.Item>
+
                 </Menu>
             </Sider>
-            
-        </React.Fragment>
         
     )
 }
